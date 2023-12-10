ab4e085926892096ddf314cb10e0a66b77727090
Pikott143 <Theunseenmaiden@gmail.com>
2023-12-10 20:17:58 +0800

    added file

---
+++ if else statment/src/pkgif/pkgelse/statment/IfElseStatment.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgif.pkgelse.statment;
+
+/**
+ *
+ * @author Aj Cervantes
+ */
+public class IfElseStatment {
+
+    public static void main(String[]args){
+        int time = 19;
+        if(time < 10) {
+            System.out.println("good morning");
+        }
+        else if (time > 18){
+            System.out.println("good day");
+        } 
+        else {
+            System.out.println("good evening");
+        }
+    }
+}
+    //keep it mind sa mga braces :>
